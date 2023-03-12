76858671975b0c0ffd5ef4478a2e30bfa447ef4e
mochi-yu <mochi.2225.it@gmail.com>
2023-03-12 10:50:08 +0900

    add: 戻るボタンの付いたヘッダーのコンポーネントを追加

---
+++ lib/components/header_with_return.dart
@@ -0,0 +1,25 @@
+ import 'package:flutter/material.dart';
+
+class HeaderWithReturn extends StatelessWidget implements PreferredSizeWidget {
+  const HeaderWithReturn({Key? key}) : super(key: key);
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.green[900],
+      title: Center(
+        child: Text(
+          "割り勘アプリ",
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back),
+        onPressed: () { print("return."); },
+      ),
+    );
+  }
+}
