b80d715e8f571b6f42562ae264d42092a0c42abe
mochi-yu <mochi.2225.it@gmail.com>
2023-03-15 22:24:45 +0900

    update: 処理選択画面のデータ管理を追加

--- lib/pages/select_operation_page.dart
+++ lib/pages/select_operation_page.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import '../components/header.dart';
 import '../components/largeButton.dart';
+import '../data/export_data.dart';
 
 class SelectOperationPage extends StatelessWidget {
   const SelectOperationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    UserData myself = ModalRoute.of(context)!.settings.arguments as UserData;
+    // print(myself.userName);
+
     return Scaffold(
       appBar: const Header(),
       body: Center(
@@ -17,12 +21,35 @@ class SelectOperationPage extends StatelessWidget {
               '操作を選んでください',
               style: Theme.of(context).textTheme.titleMedium,
             ),
+
             const SizedBox(height: 30),
-            LargeButton(label: 'レシートから入力', onPressed: () { print('レシートから入力'); }),
+            LargeButton(label: 'レシートから入力', onPressed: () {
+              print("レシートから入力");
+              myself.isHost = true;
+              /** ここでレシート読み取り画面への遷移を記述 */
+            }),
+
             const SizedBox(height: 40),
-            LargeButton(label: '手動で入力', onPressed: () { print('手動で入力'); }),
+            LargeButton(label: '手動で入力', onPressed: () {
+              print('手動で入力'); 
+              myself.isHost = true;
+              Navigator.of(context).pushNamed(
+                '/inputItemDataPage',
+                arguments: WarikanData(
+                  roomID: "",
+                  hostUser: myself,
+                  guestList: [myself],
+                  itemList: [],
+                )
+              );
+            }),
+
             const SizedBox(height: 70),
-            LargeButton(label: 'QRコードで割り勘に参加', onPressed: () { print('QRコードで割り勘に参加'); }),
+            LargeButton(label: 'QRコードで割り勘に参加', onPressed: () {
+              print('QRコードで割り勘に参加');
+              myself.isHost = false;
+              /** ここでQRコード読み取り画面への遷移を記述 */
+            }),
           ]
         ),
       ),
