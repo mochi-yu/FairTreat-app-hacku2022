c577f76b83ade876be9dfd0c860fab7bd8dc3429
mochi-yu <mochi.2225.it@gmail.com>
2023-03-15 22:25:39 +0900

    fix: ヘッダーから戻るボタンを除去

--- lib/components/header.dart
+++ lib/components/header.dart
@@ -9,6 +9,7 @@ class Header extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      automaticallyImplyLeading: false,
       backgroundColor: Colors.green[900],
       title: Center(
         child: Text(
