dad14f7cc9dc690e3a8b1261a8d819e0d34eb4a6
mochi-yu <mochi.2225.it@gmail.com>
2023-03-17 03:16:52 +0900

    update: user_dataを更新

--- lib/data/user_data.dart
+++ lib/data/user_data.dart
@@ -1,10 +1,12 @@
 class UserData {
   String userName;
+  int userID;
   bool isHost;
 
   UserData({
     required this.userName,
     required this.isHost,
+    required this.userID,
   });
 
   @override
